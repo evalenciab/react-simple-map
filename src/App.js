297471bd334639b15271a7e8dac0a1e3041a9b72
Elias Valencia <elias.itsl@gmail.com>
2021-06-07 07:11:11 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ReactTooltip from "react-tooltip";
+import MapChart from "./MapChart";
 
 function App() {
+  const [content, setTooltipContent] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+      <MapChart setTooltipContent={setTooltipContent} />
+      <ReactTooltip>{content}</ReactTooltip>
     </div>
   );
 }
---
+++ src/MapChart.js
@@ -0,0 +1,164 @@
+import React, { memo, useState } from "react";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup,
+  Sphere,
+  Graticule,
+  Marker,
+} from "react-simple-maps";
+
+const geoUrl =
+  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+const markers = [
+  {
+    markerOffset: -30,
+    name: "Buenos Aires",
+    coordinates: [-58.3816, -34.6037],
+  },
+  { markerOffset: 15, name: "La Paz", coordinates: [-68.1193, -16.4897] },
+  { markerOffset: 15, name: "Brasilia", coordinates: [-47.8825, -15.7942] },
+  { markerOffset: 15, name: "Santiago", coordinates: [-70.6693, -33.4489] },
+  { markerOffset: 15, name: "Bogota", coordinates: [-74.0721, 4.711] },
+  { markerOffset: 15, name: "Quito", coordinates: [-78.4678, -0.1807] },
+  { markerOffset: -30, name: "Georgetown", coordinates: [-58.1551, 6.8013] },
+  { markerOffset: -30, name: "Asuncion", coordinates: [-57.5759, -25.2637] },
+  { markerOffset: 15, name: "Paramaribo", coordinates: [-55.2038, 5.852] },
+  { markerOffset: 15, name: "Montevideo", coordinates: [-56.1645, -34.9011] },
+  { markerOffset: 15, name: "Caracas", coordinates: [-66.9036, 10.4806] },
+  { markerOffset: 15, name: "Lima", coordinates: [-77.0428, -12.0464] },
+];
+
+const rounded = (num) => {
+  if (num > 1000000000) {
+    return Math.round(num / 100000000) / 10 + "Bn";
+  } else if (num > 1000000) {
+    return Math.round(num / 100000) / 10 + "M";
+  } else {
+    return Math.round(num / 100) / 10 + "K";
+  }
+};
+const MapChart = ({ setTooltipContent }) => {
+  const [position, setPosition] = useState({ coordinates: [0, 0], zoom: 1 });
+
+  function handleZoomIn() {
+    if (position.zoom >= 4) return;
+    setPosition((pos) => ({ ...pos, zoom: pos.zoom * 2 }));
+  }
+
+  function handleZoomOut() {
+    if (position.zoom <= 1) return;
+    setPosition((pos) => ({ ...pos, zoom: pos.zoom / 2 }));
+  }
+
+  function handleMoveEnd(position) {
+    setPosition(position);
+  }
+  return (
+    <>
+      <ComposableMap
+        projectionConfig={{
+          rotate: [-10, 0, 0],
+          scale: 147,
+        }}
+      >
+        <ZoomableGroup
+          zoom={position.zoom}
+          center={position.coordinates}
+          onMoveEnd={handleMoveEnd}
+        >
+          {/*<Sphere stroke="#E4E5E6" strokeWidth={0.5} />
+          <Graticule stroke="#E4E5E6" strokeWidth={0.5} />*/}
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill="#EAEAEC"
+                  stroke="#D6D6DA"
+                  onMouseEnter={() => {
+                    const { NAME, POP_EST } = geo.properties;
+                    //setTooltipContent(`${NAME} — ${rounded(POP_EST)}`);
+                  }}
+                  onMouseLeave={() => {
+                    setTooltipContent("");
+                  }}
+                  style={{
+                    default: {
+                      fill: "#EAEAEC",
+                      stroke: "#D6D6DA",
+                      outline: "none",
+                    },
+                    hover: {
+                      fill: "#F53",
+                      outline: "none",
+                    },
+                    pressed: {
+                      fill: "#E42",
+                      outline: "none",
+                    },
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+          {markers.map(({ name, coordinates, markerOffset }) => (
+            <Marker
+              data-tip=""
+              onClick={() => {
+                setTooltipContent(name);
+              }}
+              key={name}
+              coordinates={coordinates}
+              style={{ cursor: "pointer" }}
+            >
+              <circle r={8} fill="#F00" stroke="#fff" strokeWidth={2} />
+              <text
+                textAnchor="middle"
+                y={markerOffset}
+                style={{
+                  fontFamily: "system-ui",
+                  fill: "#5D5A6D",
+                  fontSize: "8px",
+                }}
+              >
+                {name}
+              </text>
+            </Marker>
+          ))}
+        </ZoomableGroup>
+      </ComposableMap>
+      <div className="controls">
+        <button onClick={handleZoomIn}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="3"
+          >
+            <line x1="12" y1="5" x2="12" y2="19" />
+            <line x1="5" y1="12" x2="19" y2="12" />
+          </svg>
+        </button>
+        <button onClick={handleZoomOut}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="3"
+          >
+            <line x1="5" y1="12" x2="19" y2="12" />
+          </svg>
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default memo(MapChart);
